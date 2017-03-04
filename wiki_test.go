8042bc4dece6d5ff2d4c521af5742732e3abffb4
Akihiko Tomokane <mpc9000@gmail.com>
2017-03-04 12:46:51 +0900

    [add]add test case

--- wiki_test.go
+++ wiki_test.go
@@ -21,4 +21,24 @@ func TestViewHandler(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
+
+	expected := `<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>test</title>
+    <link rel="stylesheet" href="/static/stylesheets/main.css">
+  </head>
+  <body>
+    <h1>test</h1>
+
+<p>[<a href="/edit/test">edit</a>]</p>
+
+<div>test</div>
+  </body>
+</html>`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
